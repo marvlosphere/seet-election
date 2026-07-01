da95da3d343cc2cac940206e5370b5e3bd819fd6
Bayeri Marvelous <miraclemarvelous80@gmail.com>
2026-07-01 11:50:47 +0100

    route.ts

--- src/app/api/auth/verify/route.ts
+++ src/app/api/auth/verify/route.ts
@@ -6,7 +6,7 @@ const MAX_ATTEMPTS = 5
 const LOCK_MINUTES = 15
 const MAX_VOTES_PER_IP = 5
 const YOUR_IP_WHITELIST = [
-  '105.119.30.23', // replace with your current IP
+  '105.119.30.83', // replace with your current IP
 ]
 
 function getDb() {
